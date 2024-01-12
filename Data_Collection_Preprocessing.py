2b4610414d2ba7d4925e458d3ca989326fa7cce0
Yameen Ali <yameenshah012@gmail.com>
2024-01-13 02:31:58 +0500

    Data cleaning and preprocessing

--- Data_Collection_Preprocessing.py
+++ Data_Collection_Preprocessing.py
@@ -7,3 +7,12 @@ def gather_initial_dataset():
     # // code here...
    
     gather_initial_dataset()
+
+
+# Commit 2: Data cleaning and preprocessing
+#------------------------------------------
+# This commit contains the code for cleaning and preprocessing the dataset.
+
+def clean_and_preprocess_data():
+    # // code here...
+    clean_and_preprocess_data()
